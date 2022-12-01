6814240d52a921d392507f59797baf3d92fa98d7
colleg <colleg@cc.psu>
2022-12-01 16:15:42 +0300

    Добавлен изменённый файл main.py в репозиторий на ветку hotfix

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import random
 
 def text():
     f = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    f1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     res = ''
     for i in range(11):
-        res += random.choice(f)
+        res += random.choice(f+f1)
     return res
-
-
+    
 x = text()
 print(x)
