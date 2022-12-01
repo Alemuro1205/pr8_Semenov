be0fe4e731382b5af7148e8c1b0a4df6773b8644
colleg <colleg@cc.psu>
2022-12-01 16:21:58 +0300

    Добавлен изменённый файл с цифрави main.py в репозиторий

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def text():
     f = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    r = ['0','1','2','3','4','5','6','7','8','9']
     res = ''
     for i in range(11):
-        res += random.choice(f)
+        res += random.choice(f+r)
     return res
 
 
